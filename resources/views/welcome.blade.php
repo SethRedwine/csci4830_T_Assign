22eefb2854c97d265c628b4bcbcf68beca4d66de
Seth Redwine <sredwine@unomaha.edu>
2017-09-06 15:24:08 -0500

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
                 font-size: 24px;
             }
 
-            .links > a {
+            .links > a > span {
                 color: #636b6f;
                 padding: 0 25px;
                 font-size: 12px;
