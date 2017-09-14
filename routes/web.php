24b5d41a11d9ac15d9fb0ce3d93455df8f0034a0
Seth Redwine <sredwine@unomaha.edu>
2017-09-13 22:00:37 -0500

    db

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 
 
 Route::post('/public', function(Request $request) {
-    dd($request);
+    var_dump($request);
     $playlistName=$request->input('playlistName');
     $song0=$request->input('song0');
     $artist0=$request->input('artist0');
@@ -44,33 +44,33 @@ Route::post('/public', function(Request $request) {
     $song9=$request->input('song9');
     $artist9=$request->input('artist9');
     
-    // // DB::insert('insert into playlists (playlistName, song0, artist0, song1, artist1, song2, artist2, song3, artist3, song4, artist4, song5, artist5, song6, artist6, song7, artist7, song8, artist8, song9, artist9) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [$playlistName, $song0, $artist0, $song1, $artist1, $song2, $artist2, $song3, $artist3, $song4, $artist4, $song5, $artist5, $song6, $artist6, $song7, $artist7, $song8, $artist8, $song9, $artist9]);
-    DB::table('playlists')->insert([
-        [
-            'id'=>1,
-            'playlistName'=>$playlistName,
-            'song0'=>$song0,
-            'artist0'=>$artist0,
-            'song1'=>$song1,
-            'artist1'=>$artist1,
-            'song2'=>$song2,
-            'artist2'=>$artist2,
-            'song3'=>$song3,
-            'artist3'=>$artist3,
-            'song4'=>$song4,
-            'artist4'=>$artist4,
-            'song5'=>$song5,
-            'artist5'=>$artist5,
-            'song6'=>$song6,
-            'artist6'=>$artist6,
-            'song7'=>$song7,
-            'artist7'=>$artist7,
-            'song8'=>$song8,
-            'artist8'=>$artist8,
-            'song9'=>$song9,
-            'artist9'=>$artist9
-        ]
-    ]);
+    DB::insert('insert into playlists (playlistName, song0, artist0, song1, artist1, song2, artist2, song3, artist3, song4, artist4, song5, artist5, song6, artist6, song7, artist7, song8, artist8, song9, artist9) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [$playlistName, $song0, $artist0, $song1, $artist1, $song2, $artist2, $song3, $artist3, $song4, $artist4, $song5, $artist5, $song6, $artist6, $song7, $artist7, $song8, $artist8, $song9, $artist9]);
+    // DB::table('playlists')->insert([
+    //     [
+    //         'id'=>1,
+    //         'playlistName'=>$playlistName,
+    //         'song0'=>$song0,
+    //         'artist0'=>$artist0,
+    //         'song1'=>$song1,
+    //         'artist1'=>$artist1,
+    //         'song2'=>$song2,
+    //         'artist2'=>$artist2,
+    //         'song3'=>$song3,
+    //         'artist3'=>$artist3,
+    //         'song4'=>$song4,
+    //         'artist4'=>$artist4,
+    //         'song5'=>$song5,
+    //         'artist5'=>$artist5,
+    //         'song6'=>$song6,
+    //         'artist6'=>$artist6,
+    //         'song7'=>$song7,
+    //         'artist7'=>$artist7,
+    //         'song8'=>$song8,
+    //         'artist8'=>$artist8,
+    //         'song9'=>$song9,
+    //         'artist9'=>$artist9
+    //     ]
+    // ]);
 
     return redirect('/');
 });
