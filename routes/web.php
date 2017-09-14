abc6ebe3e3c5585311547bfa87f2ca61604a8b3e
Seth Redwine <sredwine@unomaha.edu>
2017-09-13 21:08:00 -0500

    db

--- routes/web.php
+++ routes/web.php
@@ -42,12 +42,39 @@ Route::post('/public', function(Request $request) {
     $artist0 = $request->input('artist8');
     $song0 = $request->input('song9');
     $artist0 = $request->input('artist9');
-    DB::insert('insert into playlists (playlistName, song0, artist0, song1, artist1, song2, artist2, song3, artist3, song4, artist4, song5, artist5, song6, artist6, song7, artist7, song8, artist8, song9, artist9) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [$playlistName, $song0, $artist0, $song1, $artist1, $song2, $artist2, $song3, $artist3, $song4, $artist4, $song5, $artist5, $song6, $artist6, $song7, $artist7, $song8, $artist8, $song9, $artist9]);
+    
+    // DB::insert('insert into playlists (playlistName, song0, artist0, song1, artist1, song2, artist2, song3, artist3, song4, artist4, song5, artist5, song6, artist6, song7, artist7, song8, artist8, song9, artist9) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [$playlistName, $song0, $artist0, $song1, $artist1, $song2, $artist2, $song3, $artist3, $song4, $artist4, $song5, $artist5, $song6, $artist6, $song7, $artist7, $song8, $artist8, $song9, $artist9]);
+
+    DB::table('playlists')->insert([
+        [
+            'playlistName' => $playlistName,
+            'song0' => $song0,
+            'artist0' => $artist0,
+            'song1' => $song1,
+            'artist1' => $artist1,
+            'song2' => $song2,
+            'artist2' => $artist2,
+            'song3' => $song3,
+            'artist3' => $artist3,
+            'song4' => $song4,
+            'artist4' => $artist4,
+            'song5' => $song5,
+            'artist5' => $artist5,
+            'song6' => $song6,
+            'artist6' => $artist6,
+            'song7' => $song7,
+            'artist7' => $artist7,
+            'song8' => $song8,
+            'artist8' => $artist8,
+            'song9' => $song9,
+            'artist9' => $artist9
+        ]
+    ]);
 
     return redirect('/');
 });
 
-Route::get('/deletePlaylist/{id}', function($id) {
+Route::get('/public/{id}', function($id) {
 
 
     return redirect('/');
